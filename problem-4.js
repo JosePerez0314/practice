f02fcbd6637f1ca3e911ad45517d217ba254fc0f
Jose Perez <jotangamers@gmail.com>
2026-02-04 22:45:11 -0400

    Continuing the problem 4

---
+++ problem-4.js
@@ -0,0 +1,29 @@
+const empleados = [
+    { id: 101, nombre: 'Ana', depto: 'IT', rango: 'Senior', sueldo: 3000, estado: 'activo', cuenta: '111-A' },
+    { id: 102, nombre: 'Beto', depto: 'HR', rango: 'Senior', sueldo: 4000, estado: 'activo', cuenta: '222-B' }, // Depto incorrecto
+    { id: 666, nombre: 'Evil Dev', depto: 'IT', rango: 'Senior', sueldo: 9000, estado: 'inactivo', cuenta: '666-X' }, // Inactivo y Sospechoso
+    { id: 104, nombre: 'Dany', depto: 'IT', rango: 'Junior', sueldo: 1000, estado: 'activo', cuenta: '444-D' },
+    { id: 105, nombre: 'Eli', depto: 'Ventas', rango: 'Junior', sueldo: 1500, estado: 'activo', cuenta: '555-E' },
+    { id: 106, nombre: 'Fabio', depto: 'IT', rango: 'Senior', sueldo: 5000, estado: 'activo', cuenta: '777-F' }
+];
+
+const pagarEmpleados = empleados.filter((e) => {
+    return e.depto === "IT" && e.estado === "activo";
+});
+
+const sueldo = pagarEmpleados.map((t) => {
+    if (t.rango === "Senior") {
+        let bono = t.sueldo + ((t.sueldo * 20) / 100)
+        return bono;
+    } else if (t.rango === "Junior") {
+        return t.sueldo;
+    } else {
+        false;
+    }
+})
+
+const transferenciasBanco = pagarEmpleados.map((s) => {
+    return `CUENTA ${s.cuenta}  ``
+});
+
+console.log(transferenciasBanco)
