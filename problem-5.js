185282f0da9ef505c9e66c11166ecc24378a71b0
Jose Perez <jotangamers@gmail.com>
2026-02-06 23:48:46 -0400

    continuing problem 5

---
+++ problem-5.js
@@ -0,0 +1,17 @@
+const transacciones = [
+    { id: 'tx-01', concepto: 'Venta Licencia', monto: '$1,200.00', tipo: 'ingreso', estado: 'completado' },
+    { id: 'tx-02', concepto: 'Servidor AWS', monto: '$450.50', tipo: 'gasto', estado: 'completado' },
+    { id: 'tx-03', concepto: 'Venta Fallida', monto: '$2,000.00', tipo: 'ingreso', estado: 'rechazado' },
+    { id: 'tx-04', concepto: 'Reembolso Cliente', monto: '$100.00', tipo: 'gasto', estado: 'completado' },
+    { id: 'tx-05', concepto: 'Inversión Angel', monto: '$5,000.00', tipo: 'ingreso', estado: 'pendiente' }, // Pendiente no entra en caja aún
+    { id: 'tx-06', concepto: 'Pago Nómina', monto: '$1,500.00', tipo: 'gasto', estado: 'completado' }
+];
+
+const estado = transacciones.filter((t) => t.estado === "completado" && t.tipo === "ingreso");
+
+const balanceFinal = estado.reduce((a, b) => {
+    const dinero = b.monto.replace(/,/g, '').slice(1);
+    return dineroTotal = a + parseFloat(dinero);
+}, 0);
+
+console.log(balanceFinal)
