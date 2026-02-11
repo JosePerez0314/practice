49dfd6e0928527771bf2bba3883a9389aeb64df4
Jose Perez <jotangamers@gmail.com>
2026-02-10 23:23:18 -0400

    avanzing searching

--- Async/buscador/main.js
+++ Async/buscador/main.js
@@ -3,3 +3,17 @@ const button = document.getElementById("button");
 const results = document.getElementById("js-results-container");
 
 
+button.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const inputValue = input.value;
+    if (input === "") return;
+
+    try {
+        const respuesta = await fetch(``);
+
+    } catch (error) {
+
+    }
+
+});
