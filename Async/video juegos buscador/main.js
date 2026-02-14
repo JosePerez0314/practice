4c1f8762c7809307976df71169b6e7759f89516c
Jose Perez <jotangamers@gmail.com>
2026-02-14 00:34:21 -0400

    movie searching finish

--- Async/video juegos buscador/main.js
+++ Async/video juegos buscador/main.js
@@ -2,17 +2,44 @@ const input = document.getElementById("input");
 const button = document.getElementById("button");
 const resultsContainer = document.getElementById("js-results-container");
 
-const gameCards = (gameList) => {
+const movieCards = (movieList) => {
     resultsContainer.innerHTML = "";
 
-    gameList.forEach(game => {
-        const gameCard = `
-                    <div class="card">
-                <img src="${anime.images.jpg.image_url}" alt="${anime.title}">
-                <h3>${anime.title}</h3>
-                <p>⭐ ${anime.score}</p>
-                <a href="${anime.url}" target="_blank">Ver más</a>
+    movieList.forEach(movie => {
+        const movieCard = `
+             <div class="card">
+                <img src="${movie.Poster}" alt="${movie.Title}">
+                <h3>${movie.Title}</h3>
+                <p>⭐ ${movie.Rated}</p>
+                <a href="#" target="_blank">Ver más</a>
             </div>
         `;
+
+        resultsContainer.innerHTML += movieCard;
     });
 }
+
+button.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const inputValue = input.value;
+    if (inputValue === "") return;
+
+    try {
+        const res = await fetch(`http://www.omdbapi.com/?apikey=8b15048&s=${input.value}&limit=10`);
+
+
+
+        if (!res.ok) {
+            throw new Error(`Error HTTP: ${res.status}`);
+        }
+
+        const datas = await res.json();
+        const movie = datas.Search;
+
+        movieCards(movie);
+
+    } catch (error) {
+        console.error("Error", error)
+    }
+});
